d083dc22e81df125e6c810b72e3e01dec8b3f456
kimbriah03 <85186105+kimbriah03@users.noreply.github.com>
2023-06-13 20:59:45 -0400

    Update ExpenseTotal.js

--- src/components/ExpenseTotal.js
+++ src/components/ExpenseTotal.js
@@ -1,8 +1,10 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { AppContext } from '../context/AppContext';
 
 const ExpenseTotal = () => {
     const { expenses } = useContext(AppContext);
+    const { curr, setCurr } = useState('');
+
     const totalExpenses = expenses.reduce((total, item) => {
         return (total += item.cost);
     }, 0);
@@ -10,9 +12,20 @@ const ExpenseTotal = () => {
     return (
         <div className = 'alert alert-primary'>
             <span> Spent so far: £{totalExpenses} </span>
+
+            <span>
+                <select className = 'custom-select' id = 'inputGroupSelect' onChange = {(event) => setCurr(event.target.value)}>
+                    <option defaultValue>Choose...</option>
+                    <option value = "$ Dollar" name = "dollar">$ Dollar</option>
+                    <option value = "£ Pound" name = "£ Pound">£ Pound</option>
+                    <option value = "€ Euro" name = "€ Euro">€ Euro</option>
+                    <option value = "₹ Ruppee" name = "₹ Ruppee">₹ Ruppee</option>
+                </select>
+            </span>
         </div>
+
+
     );
 };
 
 export default ExpenseTotal;
-
