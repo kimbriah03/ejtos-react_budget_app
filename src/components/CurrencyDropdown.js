51682957b641f5ecba327bf43d6743be86178380
kimbriah03 <85186105+kimbriah03@users.noreply.github.com>
2023-06-14 11:48:40 -0400

    Update CurrencyDropdown.js

--- src/components/CurrencyDropdown.js
+++ src/components/CurrencyDropdown.js
@@ -5,18 +5,17 @@ const CurrencyDropdown = (props) => {
     const { curr, setCurr } = useState('');
 
     return (
-        <div className = 'alert alert-primary'>
-            <span>
-                <select className = 'custom-select' id = 'inputGroupSelect' onChange = {(event) => setCurr(event.target.value)}>
-                    <option defaultValue>Choose...</option>
-                    <option value = "$ Dollar" name = "dollar">$ Dollar</option>
-                    <option value = "£ Pound" name = "£ Pound">£ Pound</option>
-                    <option value = "€ Euro" name = "€ Euro">€ Euro</option>
-                    <option value = "₹ Ruppee" name = "₹ Ruppee">₹ Ruppee</option>
-                </select>
-            </span>
-        </div>
+        <select className = 'alert alert-primary' id = 'inputGroupSelect' onChange = {(event) => setCurr(event.target.value)}
+            style = {
+                {paddingRight: 200}
+            }>
+                <option defaultValue>Choose...</option>
+                <option value = "$ Dollar" name = "dollar">$ Dollar</option>
+                <option value = "£ Pound" name = "£ Pound">£ Pound</option>
+                <option value = "€ Euro" name = "€ Euro">€ Euro</option>
+                <option value = "₹ Ruppee" name = "₹ Ruppee">₹ Ruppee</option>
+        </select>
     );
-}
+};
 
 export default CurrencyDropdown;
