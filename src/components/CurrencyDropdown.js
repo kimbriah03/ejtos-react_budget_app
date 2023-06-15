9db2586a920eb87a8264bb93e7a022a427f439a8
kimbriah03 <85186105+kimbriah03@users.noreply.github.com>
2023-06-14 22:40:16 -0400

    Update CurrencyDropdown.js

--- src/components/CurrencyDropdown.js
+++ src/components/CurrencyDropdown.js
@@ -1,43 +1,28 @@
 import React, { useContext, useState } from 'react';
 import { BiCaretDown } from 'react-icons/bi';
 
-function CDropdown () {
+function CDropdown ({selected, setSelected}) {
 
     const [isActive, setIsActive] = useState(false);
+    const options = ['$ Dollar', '£ Pound', '€ Euro', '₹ Ruppee']
 
     return (
         <div className = "dropdown"> 
 
-            <div className = "dropdown-btn" onClick= {e => setIsActive(!isActive)}>Choose Currency <BiCaretDown /></div>
+            <div className = "dropdown-btn" onClick= {e => setIsActive(!isActive)}>Currency: {selected} <BiCaretDown /></div>
 
             {isActive && (
                 <div className = "dropdown-content">
-                    <div className = "dropdown-item" value = "$">$ Dollar</div>
-                    <div className = "dropdown-item" value = "£">£ Pound</div>
-                    <div className = "dropdown-item" value = "€">€ Euro</div>
-                    <div className = "dropdown-item" value = "₹">₹ Ruppee</div>
+                    {options.map(option => (
+                        <div onClick = {e => {
+                            setSelected(option)
+                            setIsActive(false)}}
+        
+                        className = "dropdown-item" value = "$">{option}</div>
+                    ))}
                 </div>
             )}
         </div>
     );
 }
-/*
-const CurrencyDropdown = (props) => {
-
-    const { curr, setCurr } = useState('');
-
-    return (
-        <select className = 'alert alert-primary' id = 'inputGroupSelect' onChange = {(event) => setCurr(event.target.value)}
-            style = {
-                {paddingRight: 200}
-            }>
-                <option defaultValue>Choose...</option>
-                <option value = "$ Dollar" name = "dollar">$ Dollar</option>
-                <option value = "£ Pound" name = "£ Pound">£ Pound</option>
-                <option value = "€ Euro" name = "€ Euro">€ Euro</option>
-                <option value = "₹ Ruppee" name = "₹ Ruppee">₹ Ruppee</option>
-        </select>
-    );
-};*/
-
 export default CDropdown;
