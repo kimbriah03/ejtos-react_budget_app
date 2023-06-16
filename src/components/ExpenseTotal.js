af697566c510d4fc27aa5ce1c539c1a06341e7bd
kimbriah03 <85186105+kimbriah03@users.noreply.github.com>
2023-06-16 16:32:36 -0400

    Update ExpenseTotal.js

--- src/components/ExpenseTotal.js
+++ src/components/ExpenseTotal.js
@@ -1,27 +1,32 @@
 import React, { useContext, useState } from 'react';
 import { AppContext } from '../context/AppContext';
 
-const ExpenseTotal = () => {
+const ExpenseTotal = ({props, selected}) => {
     const { expenses } = useContext(AppContext);
-    const { curr, setCurr } = useState('');
 
     const totalExpenses = expenses.reduce((total, item) => {
         return (total += item.cost);
     }, 0);
 
+    if (selected === "$ Dollar") {
+        selected = "$";
+    }
+    else if (selected === "£ Pound") {
+        selected = "£";
+    }
+    else if (selected === "€ Euro") {
+        selected = "€";
+    }
+    else if (selected === "₹ Ruppee") {
+        selected = "₹";
+    }
+    else { //default
+        selected = "£";
+    }
+
     return (
         <div className = 'alert alert-primary'>
-            <span> Spent so far: £{totalExpenses} </span>
-
-            <span>
-                <select className = 'custom-select' id = 'inputGroupSelect' onChange = {(event) => setCurr(event.target.value)}>
-                    <option defaultValue>Choose...</option>
-                    <option value = "$ Dollar" name = "dollar">$ Dollar</option>
-                    <option value = "£ Pound" name = "£ Pound">£ Pound</option>
-                    <option value = "€ Euro" name = "€ Euro">€ Euro</option>
-                    <option value = "₹ Ruppee" name = "₹ Ruppee">₹ Ruppee</option>
-                </select>
-            </span>
+            <span> Spent so far: {selected}{totalExpenses} </span>
         </div>
 
 
