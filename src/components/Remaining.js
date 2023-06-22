fd9e15e870c17df3af45ad600235d1092815e755
kimbriah03 <85186105+kimbriah03@users.noreply.github.com>
2023-06-22 02:35:10 -0400

    Update Remaining.js

--- src/components/Remaining.js
+++ src/components/Remaining.js
@@ -1,72 +1,36 @@
-import React, { useState } from 'react';
+import React, { useContext } from 'react';
+import { AppContext } from '../context/AppContext';
 
-import 'bootstrap/dist/css/bootstrap.min.css';
+const Remaining = ({props, selected}) => {
+    const { expenses, budget } = useContext(AppContext);
 
-import "./style.css";
+    const totalExpenses = expenses.reduce((total, item) => {
+        return (total = total + item.cost);
+    }, 0);
 
-//Code to import Budget.js
-import Budget from './components/Budget';
+    const alertType = totalExpenses > budget ? 'alert-danger' : 'alert-success';
 
-// Add code to import the other components here under
-import Remaining from './components/Remaining';
-
-import ExpenseTotal from './components/ExpenseTotal';
-
-import CurrencyDropdown from './components/CurrencyDropdown';
-
-import ExpenseList from './components/ExpenseList';
-
-import AllocationForm from './components/AllocationForm';
-
-import { AppProvider } from './context/AppContext';
-
-const App = () => {
-
-    const [selected, setSelected] = useState("Choose Currency");
+    if (selected === "$ Dollar") {
+        selected = "$";
+    }
+    else if (selected === "£ Pound") {
+        selected = "£";
+    }
+    else if (selected === "€ Euro") {
+        selected = "€";
+    }
+    else if (selected === "₹ Ruppee") {
+        selected = "₹";
+    }
+    else { //default
+        selected = "£";
+    }
 
     return (
-        <AppProvider>
-            <div className='container'>
-                <h1 className='mt-3'>Company's Budget Allocation</h1>
-                    <div className='row mt-3'>
-                        {/* Add Budget component here under */} 
-                        <div className = 'col-sm'>
-                            <Budget selected = {selected}/>
-                        </div>       
-
-                        {/* Add Remaining component here under */} 
-                        <div className = 'col-sm'>
-                            <Remaining selected = {selected}/>
-                        </div>       
-
-                        {/* Add ExpenseTotal component here under */} 
-                        <div className = 'col-sm'>
-                            <ExpenseTotal selected = {selected}/>
-                        </div>
-
-                        <div className = 'col-sm'>
-                            <CurrencyDropdown selected = {selected} setSelected = {setSelected}/>
-                        </div>
-                    </div>       
-                       
-                        {/* Add ExpenseList component here under */}  
-                    <h3 className = 'mt-3'> Allocation </h3>
-                    <div className = 'row'>
-                        <div className = 'col-sm'>
-                            <ExpenseList selected = {selected} />
-                        </div>
-                    </div>          
-
-                        {/* Add AllocationForm component here under */}        
-                    <h3 className = 'mt-3'> Change Allocation </h3>
-                    <div className = 'row mt-3'>
-                        <div className = 'col-sm'>
-                            <AllocationForm selected = {selected} />
-                        </div>
-                    </div>       
-            </div>
-        </AppProvider>
+        <div className = {`alert ${alertType}`}>
+            <span> Remaining: {selected}{budget - totalExpenses}</span>
+        </div>
     );
 };
 
-export default App;
+export default Remaining;
